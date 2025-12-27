2d5c4b4e9fe98f7b6a8e71abc5a0d87e14356430
Abhijeet <demoionic2021@gmail.com>
2025-12-27 14:35:24 +0530

    initial commit featured based module skeleton

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "JetFoodRecipeAppMultiModule"
+include(":app")
+include(":receipe:data")
+include(":receipe:domain")
+include(":receipe:presentation")
+include(":core-network")
